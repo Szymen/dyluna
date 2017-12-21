c311bcd6194f0f040c3ecccf0b7793b62d7667aa
szwenda <michal-myslinski527@wp.pl>
2017-12-21 03:40:10 +0100

    Added generic view

--- dyluna/dyluna_app/views.py
+++ dyluna/dyluna_app/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, render_to_response
 from django.shortcuts import HttpResponse
+from django.http import HttpResponse
+from django.views import View
 from .models.models import User, Workshop
-from django.views.generic import TemplateView, ListView, DetailView
-
-from .forms.forms import *
-
-from .models.models import *
-
-from .models.models import User
+from .forms.forms import DietForm, UserForm
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 from django.views.generic import TemplateView
 
 
@@ -52,10 +50,35 @@ def main(request):
     return render(request, 'main.html')
 
 
-def users(request):
-    return render(request, 'users.html', {
+class Users_Display(View):
+
+    def get(self, request):
+        return render(request, 'users.html', {
         'users': User.objects.all()
-    })
+        })
+
+
+class Users_Filter_Display(ListView):
+
+    context_object_name = 'users'
+    template_name = 'menu_users_students.html'
+
+
+    def get_queryset(self):
+        self.user_role__role_name = get_object_or_404(User, name=self.kwargs['user_role'])
+        return User.objects.filter(user_role__role_name=self.user_role__role_name)
+
+    '''def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['role_user'] = self.user_role__role_name
+        return context
+
+    def get(self, request):
+        return render(request, 'menu_users_students.html', {
+            'users': self.get_context_data()
+        })'''
 
 
 def users_students(request):
